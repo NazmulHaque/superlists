20e7a4974336b0e446dda7d7bc225a8ab62a95e4
NewsCred-Nazmul <newscred@NewsCred-air-16.local>
2014-05-26 15:16:37 +0600

    ke functional_tests an app, use LiveServerTestCase"

--- lists/migrations/0001_initial.py
+++ lists/migrations/0001_initial.py
@@ -9,24 +9,24 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'Item'
-        db.create_table(u'southmig_item', (
+        db.create_table(u'lists_item', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('text', self.gf('django.db.models.fields.TextField')(default='')),
         ))
-        db.send_create_signal(u'southmig', ['Item'])
+        db.send_create_signal(u'lists', ['Item'])
 
 
     def backwards(self, orm):
         # Deleting model 'Item'
-        db.delete_table(u'southmig_item')
+        db.delete_table(u'lists_item')
 
 
     models = {
-        u'southmig.item': {
+        u'lists.item': {
             'Meta': {'object_name': 'Item'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'text': ('django.db.models.fields.TextField', [], {'default': "''"})
         }
     }
 
-    complete_apps = ['southmig']
+    complete_apps = ['lists']
--- superlists/settings.py
+++ superlists/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'lists',
     'south',
-    'southmig',
 )
 
 MIDDLEWARE_CLASSES = (
