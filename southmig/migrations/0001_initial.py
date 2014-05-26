1b933630c240dda34e8f30bf491ed869e02f067d
NewsCred-Nazmul <newscred@NewsCred-air-16.local>
2014-05-26 14:56:30 +0600

    Redirect after POST, and show all items in template

---
+++ southmig/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Item'
+        db.create_table(u'southmig_item', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('text', self.gf('django.db.models.fields.TextField')(default='')),
+        ))
+        db.send_create_signal(u'southmig', ['Item'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Item'
+        db.delete_table(u'southmig_item')
+
+
+    models = {
+        u'southmig.item': {
+            'Meta': {'object_name': 'Item'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'default': "''"})
+        }
+    }
+
+    complete_apps = ['southmig']
--- superlists/settings.py
+++ superlists/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'south',
+    'southmig',
 )
 
 MIDDLEWARE_CLASSES = (
